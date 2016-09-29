baecabcaa8f90607a29ec782e68c043354051cf3
Vittorio Meloni <vittorio.meloni@crs4.it>
2016-09-29 12:08:16 +0200

    just code cleaning and formatting

--- src/most/web/demographics/urls.py
+++ src/most/web/demographics/urls.py
@@ -11,49 +11,47 @@ from django.conf.urls import patterns, include, url
 from most.web.demographics.views import patient, city, identifier
 
 from django.contrib import admin
+
 admin.autodiscover()
 
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'most.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('')
 
 # Patient API related urls
-urlpatterns += patterns('',
-    (r'^patient/new/$', patient.new),
-    (r'^patient/get/$', patient.get),  # TODO add a "real" get api to get single patient
-    (r'^patient/filter/$', patient.filter),  # TODO add a "real" get api to get single patient
-    (r'^patient/(?P<patient_id>\d+)/edit/$', patient.edit),
-    (r'^patient/(?P<patient_id>\d+)/deactivate/$', patient.deactivate),
-    (r'^patient/(?P<patient_id>\d+)/activate/$', patient.activate),
-    (r'^patient/(?P<patient_id>\d+)/add_id/$', patient.add_identifier),
-    (r'^patient/(?P<patient_id>\d+)/remove_id/$', patient.remove_identifier),
-    (r'^patient/(?P<patient_id>\d+)/set_birth_place/$', patient.set_birth_place),
-    (r'^patient/(?P<patient_id>\d+)/set_city/$', patient.set_city),
-    #(r"^patient/delete/$", patient.delete),
-    #(r'^patient/get/all/$', 'get_all_patients'),
-    #(r'^patient/get/active/$', 'get_active_patients'),
-    #(r'^patient/get/oauth/$', 'get_active_patients_oauth'),
-    #(r'^patient/add_task_group/$', 'add_task_group_to_patient'),
-    #(r'^patient/get_task_groups/$', 'get_task_groups'),
-    #(r'^patient/(?P<patient_id>\d+)/get_medicalrecords/$', 'get_medicalrecords'),
-    #(r'^api/patient/get_contacts/$', 'get_contacts'),
+urlpatterns += patterns(
+    '',
+    (r'patient/new/$', patient.new),
+    (r'patient/get/$', patient.get),  # TODO add a "real" get api to get single patient
+    (r'patient/filter/$', patient.filter),  # TODO add a "real" get api to get single patient
+    (r'patient/(?P<patient_id>\d+)/edit/$', patient.edit),
+    (r'patient/(?P<patient_id>\d+)/deactivate/$', patient.deactivate),
+    (r'patient/(?P<patient_id>\d+)/activate/$', patient.activate),
+    (r'patient/(?P<patient_id>\d+)/add_id/$', patient.add_identifier),
+    (r'patient/(?P<patient_id>\d+)/remove_id/$', patient.remove_identifier),
+    (r'patient/(?P<patient_id>\d+)/set_birth_place/$', patient.set_birth_place),
+    (r'patient/(?P<patient_id>\d+)/set_city/$', patient.set_city),
+    # (r"patient/delete/$", patient.delete),
+    # (r'patient/get/all/$', 'get_all_patients'),
+    # (r'patient/get/active/$', 'get_active_patients'),
+    # (r'patient/get/oauth/$', 'get_active_patients_oauth'),
+    # (r'patient/add_task_group/$', 'add_task_group_to_patient'),
+    # (r'patient/get_task_groups/$', 'get_task_groups'),
+    # (r'patient/(?P<patient_id>\d+)/get_medicalrecords/$', 'get_medicalrecords'),
+    # (r'api/patient/get_contacts/$', 'get_contacts'),
 )
 
 # City API related urls
-urlpatterns += patterns('',
-    (r'^city/new/$', city.new),
-    (r'^city/get/$', city.filter),
-    (r'^city/(?P<city_id>\d+)/edit/$', city.edit),
+urlpatterns += patterns(
+    '',
+    (r'city/new/$', city.new),
+    (r'city/get/$', city.filter),
+    (r'city/(?P<city_id>\d+)/edit/$', city.edit),
 )
 
 # Identifier API related urls
-urlpatterns += patterns('',
-    (r'^identifier/new/$', identifier.new),
-    (r'^identifier/get/$', identifier.filter),
-    (r'^identifier/(?P<identifier_id>\d+)/edit/$', identifier.edit),
-    (r'^identifier/(?P<identifier_id>\d+)/get_patient/$', identifier.get_patient),
+urlpatterns += patterns(
+    '',
+    (r'identifier/new/$', identifier.new),
+    (r'identifier/get/$', identifier.filter),
+    (r'identifier/(?P<identifier_id>\d+)/edit/$', identifier.edit),
+    (r'identifier/(?P<identifier_id>\d+)/get_patient/$', identifier.get_patient),
 )
